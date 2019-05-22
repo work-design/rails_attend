5dd46923faf9468ebe477cbacaffd3c1ca5fa80d
qinmingyuan <mingyuan0715@foxmail.com>
2019-05-22 15:36:57 +0800

    config

--- lib/rails_attend/config.rb
+++ lib/rails_attend/config.rb
@@ -7,7 +7,6 @@ module RailsAttend #:nodoc:
     config.app_controller = 'ApplicationController'
     config.my_controller = 'MyController'
     config.admin_controller = 'AdminController'
-    config.panel_controller = 'PanelController'
     config.disabled_models = []
   end
 
