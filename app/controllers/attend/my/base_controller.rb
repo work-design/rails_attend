67f5fb005efd80c1718351e794adcf762bb736c3
qinmingyuan <mingyuan0715@foxmail.com>
2020-05-10 11:47:24 +0800

    base controller

--- app/controllers/attend/my/base_controller.rb
+++ app/controllers/attend/my/base_controller.rb
@@ -1,16 +1,4 @@
 class Attend::My::BaseController < RailsAttend.config.my_controller.constantize
-  before_action :require_member
 
-  def require_member
-    if current_member
-      return
-    end
-
-    if current_user.blank?
-      require_login_from_session and return
-    else
-      redirect_to my_root_url
-    end
-  end
 
 end
--- app/models/rails_attend/absence_stat.rb
+++ app/models/rails_attend/absence_stat.rb
@@ -1,6 +1,6 @@
 module RailsAttend::AbsenceStat
   extend ActiveSupport::Concern
-  
+
   included do
     attribute :year, :string
     attribute :annual_days, :float
@@ -8,6 +8,8 @@ module RailsAttend::AbsenceStat
     attribute :left_annual_days, :float
     attribute :vacation_days, :float
     attribute :details, :string, limit: 1024
+
+    belongs_to :member
   end
-  
+
 end
