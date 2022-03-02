917bfec64588313f99bf1eca5b487033fc71c435
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-02 23:07:07 +0800

    engine

--- lib/rails_attend/engine.rb
+++ lib/rails_attend/engine.rb
@@ -7,16 +7,17 @@ module RailsAttend
       "#{config.root}/app/models/absence/welfare_leave"
     ]
 
+    config.eager_load_paths += Dir[
+      "#{config.root}/app/models/absence",
+      "#{config.root}/app/models/absence/welfare_leave"
+    ]
+
     config.generators do |g|
       g.rails = {
         assets: false,
         stylesheets: false,
         helper: false,
         jbuilder: true
-      }
-      g.test_unit = {
-        fixture: true,
-        fixture_replacement: :factory_girl
       }
       g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
     end
